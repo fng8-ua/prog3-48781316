d77ed7a75ae6c4fa13fa32603949e1456abbc9c9
fng8-ua <fng8@alu.ua.es>
2021-09-24 11:17:13 +0200

    Primera modificacion del archivo Coordinate.

--- prog3-ImperialCommander/src/model/Coordinate.java
+++ prog3-ImperialCommander/src/model/Coordinate.java
@@ -1,5 +1,65 @@
 package model;
 
 public class Coordinate {
+	private int x;
+	private int y;
+	
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Coordinate(Coordinate c) {
+		this.x = c.x;
+		this.y = c.y;
+	}
+	
+	public int getX(){
+	    return x;
+	}
 
+    public int getY(){
+        return y;
+    }
+
+    public Coordinate add(int x, int y){
+
+    }
+
+    public Coordinate add(Coordinate c){
+
+    }
+
+    @Override
+	public String toString() {
+		return "Coordinate [x=" + x + ", y=" + y + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coordinate other = (Coordinate) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+    
+    
+    
 }
