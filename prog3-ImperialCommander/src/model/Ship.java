5a1615b175c3737508aa58666a0b0fce7df68440
fng8-ua <fng8@alu.ua.es>
2021-10-08 11:07:54 +0200

    declaradas las funciones de Ship, falta desarrollarlas

--- prog3-ImperialCommander/src/model/Ship.java
+++ prog3-ImperialCommander/src/model/Ship.java
@@ -1,10 +1,41 @@
 package model;
 
-public class Ship {
+import java.util.ArrayList;
+import java.util.List;
 
-	public Ship() {}
+public class Ship {
 	
-	public Side getSide() {
+	private String name;
+	private Side side;
+	private int wins;
+	private int losses;
+	private ArrayList<Fighter> fleet;
+
+	public Ship(String name, Side side) {
+		this.name = name;
+		this.side = side;
+		wins = losses = 0;
+	}
+	
+	public String getName() {return name;}
+	public Side getSide() {return side;}
+	public int getWins() {return wins;}
+	public int getLosses() {return losses;}
+	public List<Fighter> getFleetTest() {return fleet;}
+	
+	public void addFighters(String fd) {
 		
 	}
+	
+	public void updateResults(int r) {}
+	
+	public Fighter getFirstAvailableFighter(String t) {}
+	
+	public void purgeFleet() {}
+	
+	public String showFleet() {}
+	
+	public String myFleet() {}
+	
+	public String toString() {}
 }
